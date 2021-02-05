2e7afa13b7bbbbebd441c1f2075890529ce0e339
Saheli <sahelibehera2016@gmail.com>
2021-02-05 19:31:33 +0530

    React

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import UserComponent from "./components/UserComponent";
+
+export default function App() {
+  return (
+    <div className="App">
+      <UserComponent />
+    </div>
+  );
+}
